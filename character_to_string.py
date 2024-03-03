4ae5963a7279b71b56508f04d9a0d00f02b9ee3f
BytePassionDeepika <159798741+BytePassionDeepika@users.noreply.github.com>
2024-03-03 19:35:04 +0530

    Add files via upload

---
+++ character_to_string.py
@@ -0,0 +1,7 @@
+input_character = input("Enter a single character: ")
+if len(input_character) == 1:
+    result_string = str(input_character)
+    print(f"\nConverted string: {result_string}")
+else:
+    print("Please enter a single character.")
+
