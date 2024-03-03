775d96da35adfdcfd630af97823fab099bc34273
BytePassionDeepika <159798741+BytePassionDeepika@users.noreply.github.com>
2024-03-03 19:36:48 +0530

    Add files via upload

---
+++ ASCII_all_alphabet.py
@@ -0,0 +1,5 @@
+print("ASCII values of all alphabets:")
+for char in range(ord('A'), ord('Z') + 1):
+    print(f"{chr(char)}: {char}")
+for char in range(ord('a'), ord('z') + 1):
+    print(f"{chr(char)}: {char}")
